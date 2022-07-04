21c719d2ba7a52b7196df40b7a1c5a1fd9e78597
alexis691 <aguilar.you17@gmail.com>
2022-07-03 19:40:45 -0600

    :construction: model with the structure of table and migration done

---
+++ app/Models/Population.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Population extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id_nation', 'nation', 'id_year', 'year', 'population', 'slug_nation'];
+
+}
