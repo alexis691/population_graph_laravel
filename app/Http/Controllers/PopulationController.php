935d55eeba044a7cac75c174fef945bb0344ca4a
alexis691 <aguilar.you17@gmail.com>
2022-07-03 21:05:11 -0600

    :construction: Controller to get data from DB

--- app/Http/Controllers/PopulationController.php
+++ app/Http/Controllers/PopulationController.php
@@ -7,15 +7,21 @@ use Illuminate\Support\Facades\Http;
 use App\Models\Population;
 
 class PopulationController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function fromapi()
+{   
+    public function populationData()
     {
-        //
+        //Query to get all population data from population table
+        $populationData = Population::all();
+
+        //Go through data obtained from DB and insert it into arrays
+        foreach($populationData as $value){
+            $years[] = $value['year'];
+            $population[] = $value['population'];            
+            $table[] = array('year' => $value['year'], 'population' => $value['population']);
+        }
+
+        //Render view and send the obtained data
+        return view('charts.populationData', compact('table', 'years', 'population'));
     }
     
     
